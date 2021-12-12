b31b60031e50bd2f2b1c32f27cb2d4cd62f26dc2
nil <nil@yopmail.com>
2021-12-12 16:00:34 -0600

    chore: GraphQL with SpringBoot initial commit.

---
+++ src/main/java/com/difera/springql/SpringqlApplication.java
@@ -0,0 +1,13 @@
+package com.difera.springql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringqlApplication.class, args);
+	}
+
+}
